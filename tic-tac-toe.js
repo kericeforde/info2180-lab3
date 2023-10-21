6995b706b1534a1e7ee488c1b8ec762a6f14d39d
Kerice Forde <fordekerice@gmail.com>
2023-10-21 16:39:32 -0500

    Add script file

---
+++ tic-tac-toe.js
@@ -0,0 +1,8 @@
+const elements= document.querySelector("#board").querySelectorAll("div");
+for(const elem of elements){
+    elem.classList.add("square");
+    console.log(elements)
+}
+        
+
+
